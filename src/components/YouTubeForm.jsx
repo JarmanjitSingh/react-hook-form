0bba40473817a824a9b9c239b9d54d17c2a2b8c9
JarmanjitSingh <jarmanjits176@gmail.com>
2023-05-17 00:41:07 +0530

    learning new methods

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,6 +1,6 @@
 import { DevTool } from "@hookform/devtools";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray, get } from "react-hook-form";
 
 let renderCount = 0; // to check how many times component renders
 
@@ -12,13 +12,49 @@ export const YouTubeForm = () => {
     handleSubmit,
     setValue,
     formState: { errors },
-  } = useForm();
+    watch,
+    getValues,
+  } = useForm({
+    //to set a default value of the field you can also fetch data here and showit for this watch v no.12
+    defaultValues: {
+      username: "jarmanjit singh",
+      //for getting or storing the nested values in object for check submit form and check in console
+      social: {
+        twitter: "",
+        facebook: "",
+      },
+      //for storing as a array
+      phoneNumbers: ["", ""],
+      //by usefield hoook for dynamic data or fields
+      multipleAddress: [{ address: "" }],
+      //for numeric and date values set default values
+      age: 0,
+      dob: new Date(),
+    },
+  });
+
+  //for dynamic fields
+  const { fields, append, remove } = useFieldArray({
+    name: "multipleAddress",
+    control,
+  });
+
+  ///////// for watching field values
+  const watchUserName = watch("username");
+  //for watching multiple fields
+  //const watchUserName = watch(["username", "email"]);
+
+  /////// for getValues function
+  const handleGetValues = () => {
+    console.log(getValues()); //for getting all field values
+    console.log(getValues(["username"])); //for getting particular value
+  };
 
   const onSubmit = (data) => {
     console.log("form submitted", data);
   };
 
-  //this is when we show intially any value in the field
+  //this is when we show intially any value in the field and the first want is pass default values object in useform hook
   //   useEffect(() => {
   //     setValue("username", "jarmanjitsingh");
   //   }, [setValue]);
@@ -27,6 +63,9 @@ export const YouTubeForm = () => {
   return (
     <div>
       <h1>YouTube Form{renderCount / 2}</h1>
+      <h2>Watching username : {watchUserName}</h2>
+
+      {/* First part: learning register and handleSubmit vaildations, custom validations and error messages and devtool with control */}
 
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className="form-control">
@@ -56,13 +95,19 @@ export const YouTubeForm = () => {
                 message: "Invalid email format",
               },
               validate: {
-                notAdminByJarman: (fieldValue)=>{
-                    return fieldValue !== "admin@example.com" || "This is email only for internal use only"
+                notAdminByJarman: (fieldValue) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "This is email only for internal use only"
+                  );
                 },
-                notDomainSupported: (fieldValue) =>{
-                    return !fieldValue.endsWith("baddomain.com") || "This domain is not supported"
-                }
-              }
+                notDomainSupported: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  );
+                },
+              },
             })}
           />
           <p className="error">{errors.email?.message}</p>
@@ -83,7 +128,93 @@ export const YouTubeForm = () => {
           <p className="error">{errors.channel?.message}</p>
         </div>
 
+        {/* Second part : learn setting default values, storing nested objects , arrays , usefield hook for dynamic data,  */}
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="twitter" {...register("social.twitter")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <input type="text" id="facebook" {...register("social.facebook")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primary-phone">Primary Phone</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0")}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondary-phone">Secondary Phone</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1")}
+          />
+        </div>
+
+        <div>
+          <label>List of Multiple Adresses</label>
+          {fields.map((field, index) => {
+            return (
+              <div className="form-control" key={field.id}>
+                <input
+                  type="text"
+                  {...register(`multipleAddress.${index}.address`)}
+                />
+                {index > 0 && (
+                  <button type="button" onClick={() => remove(index)}>
+                    -
+                  </button>
+                )}
+              </div>
+            );
+          })}
+          <button type="button" onClick={() => append({ address: "" })}>
+            +
+          </button>
+        </div>
+
+        {/* Third part: Now this is for numeric and date values even if we set the input type number of date then u can see in console it is type string so we can set it as below */}
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            {...register("age", {
+              valueAsNumber: true,
+              required: {
+                value: true,
+                message: "age is required",
+              },
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="dob">Date of Birth</label>
+          <input
+            type="date"
+            id="dob"
+            {...register("dob", {
+              valueAsDate: true,
+              required: {
+                value: true,
+                message: "dob is required",
+              },
+            })}
+          />
+          <p className="error">{errors.dob?.message}</p>
+        </div>
         <button>Submit</button>
+        <button type="button" onClick={handleGetValues}>
+          Get Values
+        </button>
       </form>
       <DevTool control={control} />
     </div>
